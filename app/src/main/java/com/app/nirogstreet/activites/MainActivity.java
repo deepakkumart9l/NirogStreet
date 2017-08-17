c1e9e9cc3079d247b0d87e933c64bb33dcaa6b20
SainiPreet <preeti66776@gmail.com>
2017-08-17 16:08:21 +0530

    first commit

---
+++ app/src/main/java/com/app/nirogstreet/activites/MainActivity.java
@@ -0,0 +1,17 @@
+package com.app.nirogstreet.activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+import com.app.nirogstreet.R;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
